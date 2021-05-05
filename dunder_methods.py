c3c118836ed31fd1e2c35e92a2be1e21e20e45fb
lou <dustyfingers01257@gmail.com>
2021-05-05 15:15:25 -0600

    learned about dunder methods and name mangling

---
+++ dunder_methods.py
@@ -0,0 +1,27 @@
+# name
+# __name
+# __name__
+
+# dunder methods are special methods python looks for chen instnatiating an object from a class
+class Person:
+    def __init__(self):
+        self.name = "tony"
+        # private vars and methods dont actually exist in python this is just convention
+        self._secret = "hi!"
+        # try to access this __message prop i dare u
+        self.__message = "I like turtles"
+
+
+p = Person()
+print(p.name)
+print(p._secret)
+
+# python will 'mangle' this property name behind the scenes, isnt that cool?
+# print(p.__message)
+# still accessible though
+print(p._Person__message)
+
+
+# ...but why tho?
+# makes this prop particlar to THIS class...eg a classs that inherits from person can also
+# have its own 'message' property
