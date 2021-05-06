cdd3c91e9e1a75b8cd7e6a2d5205241f36253a08
lou <dustyfingers01257@gmail.com>
2021-05-06 14:04:18 -0600

    learned about class attributes in python

--- classes.py
+++ classes.py
@@ -1,9 +1,15 @@
 class User:
+    # class attributes
+    # instance attributes are unique for each instance of a class (first, last, age)
+    # class attributes are explicitly defined on the class and are the same on each instance
+    active_users = 100
+
     # the __init__ method is run upon object instantiation
     def __init__(self, first, last, age):
         self.first = first
         self.last = last
         self.age = age
+        User.active_users += 1
 
     # this is a method
     def full_name(self):
@@ -13,3 +19,5 @@ class User:
 user1 = User("Joe", "White", 42)
 user2 = User("Blanca", "Green", 22)
 user2.full_name()
+# accessed by calling the class its self
+print(User.active_users)
