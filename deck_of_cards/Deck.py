29dfeef56039f030da24f920e97138fe7e36ec2f
lou <dustyfingers01257@gmail.com>
2021-05-09 09:51:26 -0600

    wrote a simple deck class in python

---
+++ deck_of_cards/Deck.py
@@ -0,0 +1,26 @@
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
+
+
+class Deck:
+    def __init__(self):
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        values = ["A", "2", "3", "4", "5", "6",
+                  "7", "8", "9", "10", "J", "Q", "K"]
+        self.cards = [Card(value, suit) for suit in suits for value in values]
+
+    def __repr__(self):
+        return f"Deck of {self.count()} cards"
+
+    def count(self):
+        return len(self.cards)
+
+
+card_deck = Deck()
+print(card_deck)
+print(card_deck.count())
