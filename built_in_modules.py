322c8adbfafebe821c913fbe9a3f0fd3f49fd297
lou <dustyfingers01257@gmail.com>
2021-05-09 10:28:16 -0600

    reviewed using built in modules in python

---
+++ built_in_modules.py
@@ -0,0 +1,9 @@
+# to import a piece
+from random import choice
+print(choice(["apples", "bananas", "potatoes", "carrot",
+              "onion", "cherries", "farrow", "rice", "beans", "bok choy"]))
+
+# to import everything
+# import random
+# print(random.choice(["apples", "bananas", "potatoes", "carrot",
+#               "onion", "cherries", "farrow", "rice", "beans", "bok choy"]))
